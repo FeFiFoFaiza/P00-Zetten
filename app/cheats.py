f4222c62de9d1a4b0884940e1d74170df9d85972
Faiza Huda <fhuda30@stuy.edu>
2022-11-14 23:56:54 -0500

    Sql is very sensitive

--- app/cheats.py
+++ app/cheats.py
@@ -70,8 +70,8 @@ def setup():
                 """
     )
 
-   #"""  execute( """
-"""
+    execute( 
+                """
                 INSERT INTO story_db (storyID, content, usrID)
                 SELECT '1', 'Harry had a plan to pay for his school books and other supplies. When he had gone to Diagon Alley with Ron and his parents last summer before the start of his fourth year, he had been dismayed to find his vault practically empty. There had been enough gold to get new robes and his potions kit, but that was all, and he had spent the year borrowing books and school supplies from his friends. Now that the school year was over, he had three months to find a job and earn enough money to buy what he needed for his fifth year. He could not borrow things from Ron and Hermiones potions kits as that was not allowed and each kit had just enough to get a student through one year of Potions class. He needed two Galleons for that alone, which meant he needed to earn two hundred pounds. Robes would be another hundred and fifty pounds if he got used ones, and books would be about three hundred pounds if he bought them all used.', '1'
                 WHERE NOT EXISTS
@@ -79,18 +79,20 @@ def setup():
                     FROM story_db
                     WHERE storyID = '1' AND content = 'Harry had a plan to pay for his school books and other supplies. When he had gone to Diagon Alley with Ron and his parents last summer before the start of his fourth year, he had been dismayed to find his vault practically empty. There had been enough gold to get new robes and his potions kit, but that was all, and he had spent the year borrowing books and school supplies from his friends. Now that the school year was over, he had three months to find a job and earn enough money to buy what he needed for his fifth year. He could not borrow things from Ron and Hermiones potions kits as that was not allowed and each kit had just enough to get a student through one year of Potions class. He needed two Galleons for that alone, which meant he needed to earn two hundred pounds. Robes would be another hundred and fifty pounds if he got used ones, and books would be about three hundred pounds if he bought them all used.' AND usrID = '1')
                 """
-    #""" ) """
+    ) 
 
-    #""" execute( """
-"""
+    execute( 
+                """
                 INSERT INTO stories (title, summary, usrID)
-                SELECT 'An Unusual Start To The Summer', 'This is a filler which I have stolen from a good story', '1'
+                SELECT 'An Unusual Start to The Summer', 'This is a filler which I have stolen from a good story', '1'
                 WHERE NOT EXISTS
                     (SELECT title, summary, usrID
                     FROM stories
                     WHERE title = 'An Unusual Start to The Summer' AND summary = 'This is a filler which I have stolen from a good story' AND usrID = '1')
                 """
-    #""" ) """
+    )
+
+
 
     
         
